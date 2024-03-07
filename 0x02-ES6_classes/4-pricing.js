3c04298ad8303bf817a33facdefc04025c020a80
Mosesomo <moseswamboga4@gmail.com>
2024-03-07 21:04:52 +0300

    modify

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,23 +1,41 @@
+// Import the Currency class from 3-currency.js
+import Currency from './3-currency';
+
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = amount;
+    }
+
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    } else {
+      this._currency = currency;
+    }
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(amt) {
-    this._amount = amt;
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(curr) {
-    this._currency = curr;
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
   }
 
   displayFullPrice() {
@@ -25,6 +43,10 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    amount * conversionRate;
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversionRate must be numbers');
+    }
+
+    return amount * conversionRate;
   }
 }
