e9153c0c4821e9841d7992cf3e0adf4efb0876b1
Mosesomo <moseswamboga4@gmail.com>
2024-03-07 22:54:29 +0300

    inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floor) {
+    this._floors = floor;
+  }
+
+  evacuationWarningMessage() {
+    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+  }
+}
