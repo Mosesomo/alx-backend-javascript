7dd96abbfa3f9bb0768a6045f2785e72c2e3e320
Mosesomo <moseswamboga4@gmail.com>
2024-05-12 12:21:38 +0300

    debugging

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const { it, describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
+describe('calculateNumber()', () => {
     it('should return the sum of two rounded numbers (integer and integer)', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
