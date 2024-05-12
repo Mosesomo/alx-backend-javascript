f5a9b49c43109d7a00f17ca2bd22927784d135ee
Mosesomo <moseswamboga4@gmail.com>
2024-05-12 11:52:58 +0300

    unittest in js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+    it('should return the sum of two rounded numbers (integer and integer)', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return the sum of two rounded numbers (integer and float)', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return the sum of two rounded numbers (float and float)', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return the sum of two rounded numbers (float and float) rounding up', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
