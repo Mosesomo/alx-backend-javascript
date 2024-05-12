c1ea340757dcec5f6e63519fa0cace02be369ecf
Mosesomo <moseswamboga4@gmail.com>
2024-05-12 12:47:25 +0300

    still debugging

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,8 @@
 const calculateNumber = (a, b) => {
-    return Math.round(a) + Math.round(b);
+    const ar = Math.round(a);
+    const br = Math.round(b);
+    const res = ar + br;
+    return res;
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,19 +3,19 @@ const { it, describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber()', () => {
-    it('should return the sum of two rounded numbers (integer and integer)', () => {
+    it(`should return the sum of two rounded numbers (integer and integer)`, () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
 
-    it('should return the sum of two rounded numbers (integer and float)', () => {
+    it(`should return the sum of two rounded numbers (integer and float)`, () => {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
 
-    it('should return the sum of two rounded numbers (float and float)', () => {
+    it(`should return the sum of two rounded numbers (float and float)`, () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
 
-    it('should return the sum of two rounded numbers (float and float) rounding up', () => {
+    it(`should return the sum of two rounded numbers (float and float) rounding up`, () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
