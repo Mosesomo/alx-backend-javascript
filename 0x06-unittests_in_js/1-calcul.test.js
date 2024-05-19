a01f98b951fcfb5263e13c3fd5e184c953257927
Mosesomo <moseswamboga4@gmail.com>
2024-05-19 18:05:27 +0300

    combining descrition

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,27 +3,28 @@ const { it, describe } = require("mocha");
 const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber()", function() {
-    it(`should return the sum of two rounded numbers (integer and integer)`, function() {
+
+    it(`checking if numbers round`, function() {
       const res = calculateNumber("SUM", 1, 2);
       assert.strictEqual(res, 3);
     });
-    it(`should return the difference of two rounded numbers (float and float)`, function() {
+    it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 1.4, 2.2);
       assert.strictEqual(res, 1);
     });
-    it(`should return the difference of two rounded numbers (float and float)`, function() {
+    it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 4.9, 2.7);
       assert.strictEqual(res, -2);
     });
-    it(`should return the division of two rounded numbers (integer and integer)`, function() {
+    it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 4, 2);
       assert.strictEqual(res, 2);
     });
-    it(`should return "Error" if the second number is rounded to 0`, function() {
+    it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.7, 0);
       assert.strictEqual(res, "Error");
     });
-    it(`should return the division of two rounded numbers (float and float)`, function() {
+    it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.4, 4.6);
       assert.strictEqual(res, 0.2);
     });
