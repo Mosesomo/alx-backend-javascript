39794c73a6703efb4c0c45692f6ca2e8e2bdeb57
Mosesomo <moseswamboga4@gmail.com>
2024-05-23 20:38:56 +0300

    fix

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,24 +1,24 @@
 class Utils {
-    static calculateNumber (type, a, b) {
-        if (type === 'SUM') {
-            const ar = Math.round(a);
-            const br = Math.round(b);
-            const res = ar + br;
-            return res;
-        } else if (type === "SUBTRACT") {
-            const ar = Math.round(a);
-            const br = Math.round(b);
-            const res = br - ar;
-            return res;
-        } else if (type === "DIVIDE") {
-            const ar = Math.round(a);
-            const br = Math.round(b);
-            if (br === 0) {
-                return "Error";
-            }
-            const res = ar / br;
-            return res;
-        }
+    static calculateNumber(type, a, b) {
+	const an = Math.round(a);
+	const bn = Math.round(b);
+	let c = 0;
+	switch (type) {
+	case 'SUM':
+            c = an + bn;
+            break;
+	case 'SUBTRACT':
+            c = an - bn;
+            break;
+	case 'DIVIDE':
+	    if (bn === 0) {
+		c = "Error";
+	    } else {
+		c = an / bn;
+	    }
+	    break;
+	}
+	return c;
     }
 }
 
